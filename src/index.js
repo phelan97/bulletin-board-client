0947d0db5172df1e881a48b9d94acda6dcbbd9d3
Sean Phelan <seanphelan97@gmail.com>
2019-01-03 23:46:02 -0700

    add authLink

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
 import {ApolloProvider} from 'react-apollo';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {createHttpLink} from 'apollo-link-http';
+import {setContext} from 'apollo-link-context';
 import App from './components/App';
 import {endpoint, prodEndpoint} from './config';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-// TODO: authLink
+const httpLink = createHttpLink({
+  uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint
+});
+
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem('Authorization');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+});
+
 const client = new ApolloClient({
-  uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink)
 });
 
 ReactDOM.render(
