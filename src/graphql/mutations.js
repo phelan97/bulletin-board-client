50e48f368a455ec16817065494c3115a1bf0a49e
Sean Phelan <seanphelan97@gmail.com>
2019-01-03 23:35:05 -0700

    add mutations

---
+++ src/graphql/mutations.js
@@ -0,0 +1,40 @@
+import gql from 'graphql-tag';
+
+export const REGISTER = gql`
+  mutation RegisterMutation($email: String!, $password: String!) {
+    signup(email: $email, password: $password)
+  }
+`;
+
+export const LOGIN = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password)
+  }
+`;
+
+export const CREATE_BOARD = gql`
+  mutation CreateBoardMutation($title: String!) {
+    addBoard(title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+export const CREATE_LIST = gql`
+  mutation CreateListMutation($boardId: ID!, $title: String!) {
+    addList(boardId: $boardId, title: $title) {
+      id
+      title
+    }
+  }
+`;
+
+export const CREATE_CARD = gql`
+  mutation CreateCardMutation($listId: ID!, $content: String!) {
+    addCard(listId: $listId, content: $content) {
+      id
+      content
+    }
+  }
+`
