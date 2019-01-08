43e42ed8d87571cd02448d5ffeae88c98b4d51d6
Sean Phelan <seanphelan97@gmail.com>
2019-01-07 21:48:17 -0700

    add and render cards

--- src/components/list.js
+++ src/components/list.js
@@ -1,31 +1,65 @@
 import React from 'react';
-import {Query} from 'react-apollo';
+import {Query, Mutation} from 'react-apollo';
 import {LIST_CARDS} from '../graphql/queries';
+import {CREATE_CARD} from '../graphql/mutations';
 
-const List = (props) => (
-  <Query query={LIST_CARDS} variables={{listId: props.data.id}}
-    onCompleted={data => {
-      console.log('DATA', data);
-    }}>
-    {({data, error, loading}) => {
-      if(error) {
-        return <div>Error</div>
-      }
-      if(loading) {
-        return <div>Loading</div>
-      }
-      if(data.lists) {
-        const renderedCards = data.lists.map(list => {
-          return <li key={list.id}><List data={list} /></li>
-        })
-        return (
-          <ul>
-            {renderedCards}
-          </ul>
-        )
-      }
-    }}
-  </Query>
-);
+class List extends React.Component {
+  state = {
+    cardContents: ''
+  }
+  saveToState = (e) => {
+    this.setState({ [e.target.name]: e.target.value})   
+  }
+  render() {
+    return (
+      <Query query={LIST_CARDS} variables={{listId: this.props.data.id}}>
+        {({data, error, loading, refetch}) => {
+          if(error) {
+            return <div>Error</div>
+          }
+          let renderedCards;
+          if(loading) {
+            renderedCards = <li><div>Loading</div></li>
+          }
+          if(data.cards) {
+            renderedCards = data.cards.map(card => {
+              return <li key={card.id}><div>{card.content}</div></li>
+            });
+          }
+          return (
+            <React.Fragment>
+              <div>
+                <h3>{this.props.data.title}</h3>
+                <ul>
+                  {renderedCards}
+                </ul>
+              </div>
+              <Mutation mutation={CREATE_CARD}
+                variables={{content: this.state.cardContents, listId: this.props.data.id}}
+                onCompleted={data => {
+                  refetch();
+              }}>
+                {(addCard, {error}) => {
+                  return (
+                    <form onSubmit={e => {
+                      e.preventDefault();
+                      addCard();
+                      this.setState({cardContents: ''});
+                    }}>
+                      <label htmlFor="cardContents">Add new card</label>
+                      <input type="text" name="cardContents"
+                        value={this.state.cardContents} onChange={this.saveToState}></input>
+                      <button type="submit">Add card</button>
+                    </form>
+                  );
+                }}
+              </Mutation>
+            </React.Fragment>
+          );
+        }}
+        </Query>
+    );
+  }
+}
 
 export default List;
